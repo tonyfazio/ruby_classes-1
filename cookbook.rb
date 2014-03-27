8c5856af9c781c34694ae65eac3abefe77802551
Tony Fazio <antoniojfazio@gmail.com>
2014-03-27 15:53:53 -0500

    Fixed the formatting of the list in cookbook

--- cookbook.rb
+++ cookbook.rb
@@ -30,6 +30,10 @@ class Cookbook
 	def recipe_ingredients
 		@recipes.each {|x| puts "These are the ingredients for #{x.title} : #{x.ingredients} ."}
 	end
+
+	def print_cookbook
+		@recipes.each {|x| puts "These are the ingredients for #{x.title} : #{x.ingredients} . This is how you make #{x.title}: #{x.print_steps_in_cookbook}"}
+	end
 end
 
 class Recipe
@@ -64,6 +68,15 @@ class Recipe
 	def ingredients= (new_ingredients)
 		@ingredients = new_ingredients
 	end
+
+	def print_recipe
+		puts ["The title of the recipe is", @title,".", "The ingredients are: ", @ingredients,".", "The directions are:"].join(" ")
+		@steps.each_with_index {|x, i| puts "Step #{i+1}: #{x}"}
+	end
+
+	def print_steps_in_cookbook
+		@steps.each_with_index {|x, i| puts "Step #{i+1}: #{x}"}
+	end
 end
 
 mex_cuisine = Cookbook.new("Mexican Cooking")
@@ -89,3 +102,14 @@ puts mex_cuisine.recipes
 
 mex_cuisine.recipe_titles
 mex_cuisine.recipe_ingredients
+
+ puts burrito.print_recipe
+
+taco = Recipe.new("Taco", ["beef", "cheese"], ["put them together"])
+
+mex_cuisine.add_recipes(taco)
+puts mex_cuisine.recipes
+mex_cuisine.recipe_titles
+
+mex_cuisine.print_cookbook
+
